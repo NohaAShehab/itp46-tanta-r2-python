083f94afb22b562c94366744f497de1bc51046cb
Noha Shehab <nohashehab.iti@gmail.com>
2026-03-24 12:24:45 +0200

    intro to python

---
+++ intro.py
@@ -0,0 +1,81 @@
+"""
+each python file has  __doc__ --> this contains document
+of the python file
+
+Hello from intro to python  0-->
+this string is treated as a comment
+
+
+ this file illustrates how to define a variable .....
+
+"""
+
+# this is a comment in python
+
+name = 'Ahmed'
+
+Name = "Ali"
+
+print(name, Name)
+
+# if = "iti" # syntax error
+
+####################
+# print = 10
+# print("Hello")
+
+
+
+####################
+
+    # name = 'noha'   # IndentationError: unexpected indent
+
+#############################
+
+message = "Hello"
+message2 = 'World'
+
+bio = ("My name is Noha"
+       "I works at ITI ")
+
+
+print(message)
+print(message2)
+print(bio)
+
+
+bio2= '''My name is Noha
+I works at ITI
+I lives in Cairo'''
+
+print(bio2)
+
+year = 2026
+
+
+print(type(year)) # int
+print(type(bio2)) # str # detect datatype in runtime
+
+
+###############
+# change datatype of variable in the runtime ?
+
+
+
+""" this is my first string """
+
+
+
+
+
+
+
+
+
+
+print("---------------")
+print(__doc__)
+
+
+
+
