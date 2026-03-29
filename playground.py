7b899b1c0b99645a9e079efaa562b812242dc115
Noha Shehab <nohashehab.iti@gmail.com>
2026-03-29 11:42:37 +0200

    dict , ranges and more tips

---
+++ playground.py
@@ -0,0 +1,74 @@
+# for i in range(3):
+#     print(i)
+#
+# print(f"after loop {i}")
+""" ================================"""
+"""
+login = False
+for i in range(3):
+    print(i)
+    password = input("Please enter your password: ")
+    if password == "abc":
+        print("Login Successful")
+        login = True
+        break
+    if i < 2:
+        print("---- Incorrect Password, Please Try Again! ----")
+    elif i == 2:
+        print("Account is locked ")
+"""
+# if i== 2 and not login:
+#     print("---- Account is locked ")
+
+
+
+# for i in range(3):
+#     print(i)
+# else:
+#     print("--- the loop is completed")
+
+
+
+# for i in range(10):
+#     if i==6:
+#         break
+#     print(i)
+# else:
+#     print("--- the loop is completed")
+
+
+# for i in range(10):
+#     if i==6:
+#         continue
+#     print(i)
+# else:
+#     print("--- the loop is completed")
+
+
+#
+# for i in range(3):
+#     print(i)
+#     password = input("Please enter your password: ")
+#     if password == "abc":
+#         print("Login Successful")
+#         break
+#     if i < 2:
+#         print("---- Incorrect Password, Please Try Again! ----")
+# else:
+#     # this block will be executed only if ? loop completed its work
+#     print("Account is locked ")
+
+
+
+
+num = 10
+if num ==10:
+    '''pass is a null operation — when it is executed, nothing happens. 
+    It is useful as a placeholder when a statement is required 
+    syntactically, '
+     'but no code needs to be executed'''
+    pass
+
+
+print("-2222222222222")
+
